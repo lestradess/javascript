3a1ef7240661efee02fb3127b238af0ac467397d
lestrad <lestrades@gmail.com>
2022-12-15 19:07:06 +0100

    sección 15 persistencia de datos

---
+++ 15-persistencia/ejercicio-seccion15/index.js
@@ -0,0 +1,13 @@
+const nombre = "Jose";
+const apellido = "Domingo";
+
+const datos ={
+    nombre,
+    apellido,
+}
+//sessionStorage.setItem("datos", JSON.stringify(datos));
+//localStorage.setItem("datos", JSON.stringify(datos));
+
+//document.cookie = "nombreCad=Nombre;expires=" + new Date(2022, 12, 15,17,03).toUTCString()
+
+
