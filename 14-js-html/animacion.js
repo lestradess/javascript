01ccc3468d23cd7ebba9e2719e10cd7f717f59b8
lestrad <lestrades@gmail.com>
2022-12-15 18:22:27 +0100

    sección 14 gestión de eventos en DOM

---
+++ 14-js-html/animacion.js
@@ -0,0 +1,5 @@
+
+
+new TypeIt("#myElement", {
+    strings: "Con cien cañones por banda, viento en popa y a toda vela...",
+}).go();
---
+++ 14-js-html/ejercicio-seccion14/index.js
@@ -0,0 +1,8 @@
+const boton = document.getElementById("btn");
+
+boton.addEventListener("click", () => {
+    alert("click en el botón");
+})
+$("button").click(function () {
+    console.log("Hola, estoy utilizando jQuery")
+})
